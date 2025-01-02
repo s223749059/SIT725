e7f7378a304650b510190f4f005af46e98bf0e01
Minh Tho DUONG <s223749059@deakin.edu.au>
2025-01-03 00:34:09 +1100

    sit725 - practice week7

---
+++ week7/public/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.modal').modal();
+
+    $('#button_intro_video').click(() => {
+        window.open("https://www.youtube.com/watch?v=VYyb7VO5DBA")
+    })
+
+    $('#button_drawing').click(() => {
+        window.open("/drawing.html")
+    })
+});
---
+++ week7/startServerWithMongo.js
@@ -0,0 +1,90 @@
+let express = require('express');
+let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb://localhost:27017";
+// const uri = "mongodb+srv://faisalalam:w7dwNuoISsPyESk8@cluster0.gngbcep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const uri = "mongodb+srv://s223749059:2fICYltL4sfxoG1M@cluster0.tjcoayf.mongodb.net/"
+let port = process.env.port || 3000;
+let collection;
+
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log(collection);
+    } catch(ex) {
+        console.error(ex);
+    }
+}
+
+app.get('/', function (req,res) {
+    res.render('admin.html');
+});
+
+app.get('/api/cats', (req,res) => {
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200, data:result, message:'get all cats successful'});
+        }
+    });
+});
+
+app.post('/api/cat', (req,res)=>{
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({statusCode:201, data:result, message:'success'});
+        }
+    });
+});
+
+function postCat(cat,callback) {
+    collection.insertOne(cat,callback);
+}
+
+function getAllCats(callback){
+    collection.find({}).toArray(callback);
+}
+
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    setInterval(()=>{
+        x=parseInt(Math.random()*10);
+        socket.emit('number', x);
+        console.log('Emmiting Number '+x);
+    }, 1000)
+});
+
+http.listen(port, ()=>{
+    console.log('express server started');
+    runDBConnection();
+});
+
+// io.on('connection', function (socket) {
+// 	socket.on('draw', function (data) {
+// 		socket.broadcast.emit('draw', data);
+// 	});
+// });
